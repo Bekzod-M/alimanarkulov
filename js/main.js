beef979cf15518a744923cfb151e513375cd1f73
Bekzod-M <40116379+Bekzod-M@users.noreply.github.com>
2020-10-17 18:26:28 +0500

    alimanarkulov

---
+++ js/main.js
@@ -0,0 +1,137 @@
+// выпадающее меню хедер (моб)
+let isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
+let bodyM = document.querySelector('body');
+if (isMobile.any()) {
+  bodyM.classList.add('touch');
+  let arrow = document.querySelectorAll('.arrow');
+  for (i = 0; i < arrow.length; i++) {
+    let thisLink = arrow[i].previousElementSibling;
+    let subMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
+
+    thisLink.classList.add('parent');
+    arrow[i].addEventListener('click', function () {
+      subMenu.classList.toggle('open');
+      thisArrow.classList.toggle('active');
+    });
+  }
+} else {
+  bodyM.classList.add('mouse');
+}
+
+
+// слайдер
+$('.main-slider__content').slick({
+  arrows: true,
+  easing: 'linear',
+  centerMode: true,
+  centerPadding: '120px',
+  variableWidth: true,
+  slidesToShow: 1,
+  useTransform: false,
+  responsive: [{
+    breakpoint: 800,
+    settings: {
+      mobileFirst: true,
+      adaptiveHeight: true,
+      centerMode: false,
+      centerPadding: '0%',
+      variableWidth: false,
+      arrows: false
+    }
+  }]
+});
+
+$('.products-slider__content').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+//вставка видео через youtube
+function r(f) {
+  /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f()
+}
+r(function () {
+  if (!document.getElementsByClassName) {
+    // Поддержка IE8
+    var getElementsByClassName = function (node, classname) {
+      var a = [];
+      var re = new RegExp('(^| )' + classname + '( |$)');
+      var els = node.getElementsByTagName("*");
+      for (var i = 0, j = els.length; i < j; i++)
+        if (re.test(els[i].className)) a.push(els[i]);
+      return a;
+    }
+    var videos = getElementsByClassName(document.body, "youtube");
+  } else {
+    var videos = document.getElementsByClassName("youtube");
+  }
+  var nb_videos = videos.length;
+  for (var i = 0; i < nb_videos; i++) {
+    // Находим постер для видео, зная ID нашего видео
+    videos[i].style.backgroundImage = 'url(http://i.ytimg.com/vi/' + videos[i].id + '/sddefault.jpg)';
+    // Размещаем над постером кнопку Play, чтобы создать эффект плеера
+    var play = document.createElement("div");
+    play.setAttribute("class", "play");
+    videos[i].appendChild(play);
+    videos[i].onclick = function () {
+      // Создаем iFrame и сразу начинаем проигрывать видео, т.е. атрибут autoplay у видео в значении 1
+      var iframe = document.createElement("iframe");
+      var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+      if (this.getAttribute("data-params")) iframe_url += '&' + this.getAttribute("data-params");
+      iframe.setAttribute("src", iframe_url);
+      iframe.setAttribute("frameborder", '0');
+      // Высота и ширина iFrame будет как у элемента-родителя
+      iframe.style.width = this.style.width;
+      iframe.style.height = this.style.height;
+      // Заменяем начальное изображение (постер) на iFrame
+      this.parentNode.replaceChild(iframe, this);
+    }
+  }
+});
